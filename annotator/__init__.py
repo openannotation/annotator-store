970df235e02a14ededd875237b4149c76bc430b2
Nick Stenning <nick@whiteink.com>
2012-01-30 19:44:35 +0000

    Version bump

--- annotator/__init__.py
+++ annotator/__init__.py
@@ -3,7 +3,7 @@ Backend for web annotation.
 
 @copyright: (c) 2006-2012 Open Knowledge Foundation
 """
-__version__ = '0.5.1'
+__version__ = '0.6'
 __license__ = 'MIT'
 __author__ = 'Rufus Pollock and Nick Stenning (Open Knowledge Foundation)'
 
