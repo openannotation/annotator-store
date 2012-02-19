b958f983539e5e5c6021db49d3b5608aed3d6fad
Nick Stenning <nick@whiteink.com>
2012-02-19 21:05:07 +0000

    Upgrade console.py to provide an authenticated environment

--- console.py
+++ console.py
@@ -1,13 +1,25 @@
 from IPython import embed
 
+from flask import request
+
 import annotator
+from annotator import auth
 
 app = annotator.create_app()
-app.test_request_context().push()
+ctx = app.test_request_context()
+ctx.push()
 
 from annotator import model
 
 db = app.extensions['sqlalchemy'].db
 es = app.extensions['pyes']
+
+token = auth.generate_token('annotateit', 'admin')
+
+ctx.pop()
+
+# Push new test context with auth headers attached
+ctx = app.test_request_context(headers=auth.headers_for_token(token).items())
+ctx.push()
 
 embed(display_banner=False)
