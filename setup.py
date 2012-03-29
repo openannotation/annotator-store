df9a3c7d0cd5920c09bba6230c37f877762fb740
Nick Stenning <nick@whiteink.com>
2012-03-29 15:02:26 +0100

    Bump version -> v0.7.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'annotator',
-    version = '0.7.5',
+    version = '0.7.6',
     packages = find_packages(),
 
     install_requires = [
