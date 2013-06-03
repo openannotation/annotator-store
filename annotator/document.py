39f07969bc4918874d659bbe9c8fff9e2c6a98a9
Randall Leeds <randall.leeds@gmail.com>
2013-06-02 20:55:17 -0700

    trailing whitespace

--- annotator/document.py
+++ annotator/document.py
@@ -16,8 +16,8 @@ MAPPING = {
     'dc': {
         'type': 'nested',
         'properties': {
-            # by default elastic search will try to parse this as 
-            # a date but unfortunately the data that is in the wild 
+            # by default elastic search will try to parse this as
+            # a date but unfortunately the data that is in the wild
             # may not be parsable by ES which throws an exception
             'date': {'type': 'string', 'index': 'not_analyzed'}
         }
