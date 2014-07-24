e5ac0f7763a7c16d9f6685181d5b8498ecac2c6c
Gerben <gerben@treora.com>
2014-07-24 12:25:48 -0700

    Add tests for op_type

--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -64,3 +64,23 @@ class TestModel(object):
         conn.get.side_effect = raise_exc
         o = self.Model.fetch(123)
         assert_equal(o, None)
+
+    @patch('annotator.elasticsearch.elasticsearch.Elasticsearch')
+    def test_op_type_create(self, es_mock):
+        """Test if operation type is 'create' in absence of an id field"""
+        m = self.Model(bla='blub')
+        m.save()
+
+        conn = es_mock.return_value
+        call_kwargs = conn.index.call_args_list[0][1]
+        assert call_kwargs['op_type'] == 'create', "Operation should be: create"
+
+    @patch('annotator.elasticsearch.elasticsearch.Elasticsearch')
+    def test_op_type_index(self, es_mock):
+        """Test if operation type is 'index' when an id field is present"""
+        m = self.Model(bla='blub', id=123)
+        m.save()
+
+        conn = es_mock.return_value
+        call_kwargs = conn.index.call_args_list[0][1]
+        assert call_kwargs['op_type'] == 'index', "Operation should be: index"
