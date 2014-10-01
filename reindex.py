683ac1cbc7de06afcc2fd91d52c61f7f1ef5d1c3
Gerben <gerben@treora.com>
2014-10-01 19:56:03 +0300

    Add --magic option to do in-place reindexing

--- reindex.py
+++ reindex.py
@@ -8,11 +8,11 @@ from annotator.reindexer import Reindexer
 description = """
 Reindex an elasticsearch index.
 
-If old_index and new_index are given the same value, a new name will be
-generated for new_index, the old_index will be deleted, and the newly created
-index will be aliased to old_index, as to pretend an in-place reindexing. If
-old_index was an alias itself, it will be reassigned to target the new index,
-and its old target will remain untouched.
+If old_index and new_index are given the same value and --magic is enabled,
+a new name will be generated for new_index, the old_index will be deleted,
+and the newly created index will be aliased to old_index, as to pretend an
+in-place reindexing. If old_index was an alias itself, it will be reassigned
+to target the new index, and its old target will remain untouched.
 
 WARNING: Documents that are created while reindexing may be lost!
 """
@@ -22,16 +22,28 @@ def main(argv):
     argparser.add_argument('host', help="Elasticsearch server, host[:port]")
     argparser.add_argument('old_index', help="Index to read from")
     argparser.add_argument('new_index', help="Index to write to")
+    argparser.add_argument('--magic', action='store_true',
+                           help="Allow in-place reindexing by juggling with "
+                                "aliases")
     args = argparser.parse_args()
 
     host = args.host
     old_index = args.old_index
     new_index = args.new_index
+    magic = args.magic
+
+    if magic:
+        if old_index != new_index:
+            raise argparse.ArgumentError("For --magic in-place reindexing, "
+                                 "old_index and new_index should be the same.")
 
     conn = Elasticsearch([host])
 
     reindexer = Reindexer(conn, interactive=True)
-    reindexer.reindex(old_index, new_index)
+    if magic:
+        reindexer.reindex_in_place(old_index)
+    else:
+        reindexer.reindex(old_index, new_index)
 
 
 if __name__ == '__main__':
