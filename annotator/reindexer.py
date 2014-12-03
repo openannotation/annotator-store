af5053cdf066a850df7c5e5ca627f6f00c6f1b22
Randall Leeds <randall@bleeds.info>
2014-12-02 21:20:38 -0800

    Reindexer shouldn't fail when target exists
    It's a common use case to reindex without creating the target. For
    instance, after reindexing, an operator may cut over new writes and
    then reindex again to ensure any writes that ocurred during the
    reindex are replayed to the new index.

--- annotator/reindexer.py
+++ annotator/reindexer.py
@@ -25,10 +25,11 @@ class Reindexer(object):
             raise ValueError("Index {0} does not exist!".format(old_index))
 
         if conn.indices.exists(new_index):
-            raise ValueError("Index {0} already exists!".format(new_index))
-
-        # Create the new index with (presumably) new mapping config
-        conn.indices.create(new_index, body=self.get_index_config())
+            self._print("Index {0} already exists. "
+                        "The mapping will not be changed.".format(new_index))
+        else:
+            # Create the new index with (presumably) new mapping config
+            conn.indices.create(new_index, body=self.get_index_config())
 
         # Do the actual reindexing.
         self._print("Reindexing {0} to {1}..".format(old_index, new_index))
